107c52a0b5ae3317aebd3512d10f0a1b74b1bd25
Nicolae Totalca <totalca@yahoo.com>
2018-11-02 14:10:22 +0000

    Ingredient added to component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Drinks from './component/drinks';
+import Ingredient from './component/ingredient';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Drink Pricing</h1>
+      <h1>Manage Ingredient</h1>
+      <Ingredient />
+      <h1>Manage Drinks</h1>
       <Drinks />
 
 
---
+++ src/component/ingredient/ingredient.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Ingredient extends Component {
+    render() {
+        return (
+          <div>This is Ingredient</div>
+        );
+    }
+}
+
+export default Ingredient;
