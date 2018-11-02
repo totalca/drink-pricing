4d31c2b09d95bb7c8e1407643e29d1653f38b882
Nicolae Totalca <totalca@yahoo.com>
2018-11-02 15:15:56 +0000

    Ingredient data imported from JSON

--- src/component/ingredient/ingredient.jsx
+++ src/component/ingredient/ingredient.jsx
@@ -1,9 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { BASE_URL } from './constant'
 
 class Ingredient extends Component {
+    state = {
+        ingredient: []
+    }
+
+    componentDidMount() {
+        axios
+            .get(`${BASE_URL}ingredient`)
+            .then(({data}) => {
+                console.log(data);
+                this.setState({ingredient: data})
+            })
+
+    }
+
+    renderIngredientList = () => {
+        return this.state.ingredient
+            .map(({id, name, unit, currency, price, quantity}, key) =>
+                <tr key={key}>
+                    <td>{id}</td>
+                    <td>{name}</td>
+                    <td>{unit}</td>
+                    <td>{currency}{price}</td>
+                    <td>{quantity}</td>
+                    <td>Act</td>
+                </tr>
+            );
+    }
     render() {
         return (
-          <div>This is Ingredient</div>
+            <table>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Name</td>
+                        <td>Unit</td>
+                        <td>Price</td>
+                        <td>Quantity</td>
+                        <td>Actions</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.renderIngredientList()}
+                </tbody>
+                </table>
         );
     }
 }
